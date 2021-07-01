937d4b9437a3135a35c502fda27203dc9d932ec1
Dane Isburgh <daneisburgh@gmail.com>
2021-07-01 18:13:26 -0400

    refactor: asset file paths script
    Add logic to ignore certain files and directories.

--- assets-file-paths.js
+++ assets-file-paths.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 
+const ignoreFiles = [
+    '.DS_Store'
+];
+
 const getAllFilePaths = (directory, filePaths) => {
     filePaths = filePaths || [];
     const files = fs.readdirSync(directory);
 
     for (const file of files) {
-        const filePath = `${directory}/${file}`;
+        if (!ignoreFiles.includes(file)) {
+            const filePath = `${directory}/${file}`;
 
-        if (fs.statSync(filePath).isDirectory()) {
-            filePaths = getAllFilePaths(filePath, filePaths);
-        } else {
-            filePaths.push(filePath.replace('src/', ''));
+            if (fs.statSync(filePath).isDirectory()) {
+                filePaths = getAllFilePaths(filePath, filePaths);
+            } else {
+                filePaths.push(filePath.replace('src/', ''));
+            }
         }
     }
 
